66cbf0c56344cc8030592545ac54bf5fe24a5630
Kenneth Reitz <me@kennethreitz.com>
2012-06-09 03:32:53 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""
+distutils/setuptools install script. See inline comments for packaging documentation.
+"""
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+    # hush pyflakes
+    setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+requires = ['boto>=2.2.0']
+
+
+setup(
+    name='dynamo',
+    version='0.0.1',
+    description='Python HTTP for Humans.',
+    long_description=open('README.rst').read() + '\n\n' +
+                     open('HISTORY.rst').read(),
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='http://python-requests.org',
+    py_modules=['dynamo'],
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    install_requires=requires,
+    license=open('LICENSE').read(),
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.0',
+        # 'Programming Language :: Python :: 3.1',
+    ),
+)
